2cfc3dc62f503be0d71e66d764a4c71617aec5c5
Barni6264 <bende.barnabas2005@gmail.com>
2025-09-03 01:24:22 +0200

    Fixed almost all issues in the background scripts

--- scripts/discord_injected.js
+++ scripts/discord_injected.js
@@ -1,32 +1,38 @@
 const TYPES_REQUEST = "request";
 const TYPES_RESPONSE = "response";
+const TYPES_PING = "u alive?";
 
 const CRYPT_REASON_EN = "encrypt";
 const CRYPT_REASON_DE = "decrypt";
 
-const PING_REQUEST = "u alive?";
+const DIRECTION_I2C = "injected2content";
+const DIRECTION_C2I = "content2injected";
 
-const shield = document.currentScript.dataset.shield;
+const shield = document.currentScript.dataset["shield"];
 
-//#region content script comms
+//#region Content script comms
 window.addEventListener("message", async (message) => {
-    if (message.shield !== shield) {
-        console.warn(`Mismatch between shared secrets. Expected: ${shield}; Actual: ${message.shield}`);
+    const data = message.data;
+    if (data.direction !== DIRECTION_C2I) return;
+
+    if (data.shield !== shield) {
+        console.warn(`Mismatch between shared secrets. Expected: ${shield}; Actual: ${data.shield}`);
         return;
     }
 
-    switch (message.type) {
+    switch (data.type) {
+        case TYPES_PING:
+            sendToContent({
+                type: TYPES_PING
+            });
+            break;
         case TYPES_REQUEST:
-            if (message.data === PING_REQUEST) {
-                sendToContent({
-                    type: TYPES_RESPONSE,
-                    data: PING_REQUEST
-                });
-            }
+            // Reserved for later use
             break;
         case TYPES_RESPONSE:
-            messageMap.get(message.data.id)(message.data); // resolves { content, success }
-            messageMap.delete(message.data.id);
+            // Resolve { content, success }
+            messageMap.get(data.id)(data);
+            messageMap.delete(data.id);
             break;
         default:
             console.error("Invalid message", message);
@@ -36,11 +42,12 @@ window.addEventListener("message", async (message) => {
 
 function sendToContent(message) {
     message.shield = shield;
+    message.direction = DIRECTION_I2C;
     window.postMessage(message);
 }
 //#endregion
 
-//#region messages
+//#region Messages
 const messageMap = new Map();
 function onIncomingMessage(message) {
     const id = crypto.getRandomValues(new Uint32Array(8)).join("+");
@@ -48,11 +55,9 @@ function onIncomingMessage(message) {
         messageMap.set(id, resolve);
         sendToContent({
             type: TYPES_REQUEST,
-            data: {
-                reason: CRYPT_REASON_DE,
-                id: id,
-                content: message,
-            }
+            reason: CRYPT_REASON_DE,
+            id: id,
+            content: message
         });
     });
 }
@@ -62,16 +67,15 @@ function onOutgoingMessage(message) {
         messageMap.set(id, resolve);
         sendToContent({
             type: TYPES_REQUEST,
-            data: {
-                reason: CRYPT_REASON_EN,
-                id: id,
-                content: message,
-            }
+            reason: CRYPT_REASON_EN,
+            id: id,
+            content: message
         });
     });
 }
+//#endregion
 
-//#region overwrite outgoing
+//#region Overwrite outgoing
 const originalOpen = XMLHttpRequest.prototype.open;
 const originalSend = XMLHttpRequest.prototype.send;
 XMLHttpRequest.prototype.open = function (method, url, ...rest) {
@@ -80,87 +84,105 @@ XMLHttpRequest.prototype.open = function (method, url, ...rest) {
     return originalOpen.call(this, method, url, ...rest);
 };
 XMLHttpRequest.prototype.send = function (body) {
-    try {
-        if (this._url.includes("/api/v9/channels/") && this._url.endsWith("/messages")) {
-            console.log("Intercepted message.");
+    if (!this._url.includes("/api/v9/channels/") || !this._url.endsWith("/messages") || !body) {
+        originalSend.call(this, body);
+        return;
+    }
 
-            if (body) {
-                let parsed;
-                try { parsed = JSON.parse(body); } catch { }
-                if (parsed && parsed.content) {
-                    console.log("Parsing...");
-                    onOutgoingMessage(parsed.content).then((newMessage) => {
-                        console.log("Finished parsing.");
-                        parsed.content = newMessage;
-                        body = JSON.stringify(parsed);
-                        originalSend.call(this, body);
-                    });
-                }
-            } else {
-                return originalSend.call(this, body);
+    console.log("Intercepted message.");
+    let parsed;
+    try {
+        parsed = JSON.parse(body);
+    } catch {
+        originalSend.call(this, body);
+        return;
+    }
+
+    if (parsed.content) {
+        console.log("Encrypting...");
+        onOutgoingMessage(parsed.content).then((newMessage) => {
+            console.log("Attempting to send...");
+            if (newMessage.success === true) {
+                parsed.content = newMessage.content;
+                originalSend.call(this, JSON.stringify(parsed));
+                console.log("Finished sending modified message.");
             }
-        } else {
-            return originalSend.call(this, body);
-        }
-    } catch (err) {
-        console.error("XHR hook died", e);
+        });
     }
 };
 //#endregion
 
-//#region overwrite incoming
-const observer = new MutationObserver(async muts => {
-    for (const mut of muts) {
-        // new nodes (*1)
-        for (const node of mut.addedNodes) {
-            if (node.nodeType === Node.ELEMENT_NODE && node.nodeName === "LI") iterateNodeTree(node);
-        }
+//#region Decrypt messages in the HTML (including incoming and outgoing; basically all rendered messages)
+const observer = new MutationObserver(async mutations => {
+    for (const mutation of mutations) {
+        // When new nodes (possibly messages or nodes that hold message nodes) are added
+        for (const node of mutation.addedNodes)
+            iterateNodeTree(node);
 
-        // node text content override
-        if (mut.type === "characterData" && isMessageNode(mut.target.parentNode) && mut.target.parentNode.dataset.shield === shield) {
-            console.log("Text node changed.");
-            mut.target.nodeValue = await onIncomingMessage(mut.target.nodeValue);
+        // When an existing encrypted message content got modified instead
+        /* Note
+            try to decrypt again
+                + new encrypted message; displays a fresh green html
+                - keep the old green, TODO
+        */
+        if (isDecryptedNode(mutation.target.parentNode)) {
+            console.log("Existing node modified. Rebuilding span...");
+            modifyNode(mutation.target.parentNode);
         }
     }
 });
-function iterateNodeTree(node) { // for new nodes (*1)
-    node.childNodes.forEach(async (n) => {
-        if (!isMessageNode(n)) {
-            iterateNodeTree(n);
+function iterateNodeTree(n) {
+    n.childNodes.forEach(async (node) => {
+        if (!isTextMessageNode(node)) {
+            iterateNodeTree(node);
             return;
         }
-
-        console.log("Message node found.");
-        const spansParent = n.parentElement;
-
-        // "loading"
-        spansParent.innerHTML = "";
-        spansParent.appendChild(buildLoadingHtml());
-
-        const data = await onIncomingMessage(n.textContent);
-        if (data.success === true) { // "encrypted"
-            spansParent.innerHTML = "";
-            const newSpan = buildHtmlEncrypted(data.content);
-            spansParent.appendChild(newSpan);
-
-            // also use shield as a flag for elements decrypted (these will not be ignored by the text node override fix)
-            newSpan.dataset.shield = shield;
-        } else { // plain text original message
-            n.textContent = data.content;
-        }
+        console.log("Fresh message node found. Modifying...");
+        modifyNode(node);
     });
 }
-function isMessageNode(node) {
-    return node.nodeType === Node.ELEMENT_NODE && node.textContent && node.parentElement.id.startsWith("message-content-");
+
+// Modify a <span> node that may or may not contain encrypted text
+async function modifyNode(node) {
+    const spanParent = node.parentElement;
+    const textContent = node.textContent;
+    const fullSpanParentContent = spanParent.innerHTML;
+
+    // Decrypt
+    const dataPromise = onIncomingMessage(textContent);
+
+    // Display "Loading message..." text
+    spanParent.innerHTML = "";
+    spanParent.appendChild(buildLoadingHtml());
+
+    const data = await dataPromise;
+    // If the message is encrypted
+    if (data.success === true) {
+        console.log("Patching node...");
+        spanParent.innerHTML = "";
+        const newSpan = buildHtmlEncrypted(data.content);
+
+        // Use the shield as a flag to mark spans, and tell the observer to ignore the update (to prevent an infinite loop)
+        newSpan.dataset.shield = shield;
+
+        spanParent.appendChild(newSpan);
+
+    } else {
+        // Restore whole old content
+        console.log("Restoring node.");
+        spanParent.innerHTML = fullSpanParentContent;
+    }
 }
 
-observer.observe(document.body,
-    { childList: true, subtree: true, characterData: true }
-);
-//#endregion
+function isTextMessageNode(node) {
+    return node.nodeType === Node.ELEMENT_NODE && node.textContent && node.parentElement.id.startsWith("message-content-");
+}
+function isDecryptedNode(node) {
+    return node.dataset["shield"] === shield;
+}
 //#endregion
 
-//#region build htmls
+//#region HTML builders
 function buildHtmlEncrypted(message) {
     const outer = document.createElement("span");
     outer.style.border = "2px solid #008800";
@@ -178,15 +200,11 @@ function buildHtmlEncrypted(message) {
     label.style.margin = "0px 10px 0px 0px";
     label.textContent = "E2EE";
 
-    const content = document.createElement("span");
-    content.textContent = message;
-
     outer.appendChild(label);
-    outer.appendChild(content);
+    outer.append(message);
 
     return outer;
 }
-
 function buildLoadingHtml() {
     const label = document.createElement("span");
     label.style.color = "#777777";
@@ -194,5 +212,10 @@ function buildLoadingHtml() {
 
     return label;
 }
+//#endregion
 
+// Main
 console.log("Hello from injected script!");
+observer.observe(document.body,
+    { childList: true, subtree: true, characterData: true }
+);
